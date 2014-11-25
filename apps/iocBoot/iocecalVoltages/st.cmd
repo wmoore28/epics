d5f13fcc79a087f1ab15b9d9f939aebf95fd7c2b
Wesley Moore <wmoore@jlab.org>
2014-11-25 12:06:31 -0500

    iocecalVoltages: updated for new CAEN_HV paths and other minor mods

--- apps/iocBoot/iocecalVoltages/st.cmd
+++ apps/iocBoot/iocecalVoltages/st.cmd
@@ -1,29 +1,23 @@
-#!/usr/local/clas12/release/0.1/epics/drivers/CAEN_HV/level0/IocShell/O.linux-x86/ioccaen
-# TODO: correct paths as needed (nerses, wmoore)
+#!../../bin/linux-x86/ioccaen
 
-< envPaths.cmd
+< envPaths
 cd ${TOP}
 
 ## Register all support components
 dbLoadDatabase("dbd/ioccaen.dbd")
 ioccaen_registerRecordDeviceDriver(pdbbase)
 
+# call to run sy1527Init()
 Init_CAEN()
 
-# hvhps1
-Start_CAEN(1, "129.57.167.100")
-## hvecal1
-#Start_CAEN(2, "129.57.167.53")
-# hvecal4
-Start_CAEN(2, "129.57.167.190")
+# call to run sy1527Start(), sy1527GetMap(), sy1527PrintMap()
+Start_CAEN(1, "hvhps1")
+Start_CAEN(2, "hvecal4")
 
 ## Load record instances
-cd ${DB}
-dbLoadRecords("ecal_01.db")
-dbLoadRecords("bm_01.db")
-dbLoadRecords("bm_02.db")
+dbLoadRecords("db/ecal_01.db")
+dbLoadRecords("db/bm_01.db")
+dbLoadRecords("db/bm_02.db")
 
-#cd ${TOP}/iocBoot/${IOC}
-cd /home/nerses/ECAL/iocBoot/iocCAEN_HV
+cd ${TOP}/iocBoot/${IOC}
 iocInit()
-
