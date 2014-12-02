0c1fc635c1c8d49a713ce9cec02e69371518249d
Wesley Moore <wmoore@jlab.org>
2014-12-02 13:44:16 -0500

    flasherApp: added automated scripts for running blue|red sequences

---
+++ apps/flasherApp/medm/flasher_start_color_seq.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#
+# Steps for running the [blue|red] sequence.
+#
+
+# Assign PV prefixes
+TOP="B_HPS_ECAL_FLASHER_TOP"
+BOT="B_HPS_ECAL_FLASHER_BOT"
+
+usage() { echo "Usage: $0 -c <blue|red>" 1>&2; exit 1; }
+
+while getopts "c:" arg; do
+	case $arg in
+	c) 
+		COLOR="$(echo ${OPTARG} | tr '[:upper:]' '[:lower:]')"
+	   	;;
+	*)
+		usage
+		;;
+	esac
+done
+shift $((OPTIND-1))
+
+# Color and Clock must be defined
+if [ -z "${COLOR}" ]; then
+	usage
+fi
+
+# Fix capitalization for usage later
+if [ "$COLOR" = "blue" ]; then
+	COLOR="Blue"
+elif [ "$COLOR" = "red" ]; then
+	COLOR="Red"
+else
+	usage
+fi
+
+# Setup/Run TOP
+caput ${TOP}:SET_COLOR $COLOR
+caput ${TOP}:DATA_FILE_LOAD HPSFlasher1${COLOR}Channels.dat
+./flasher_load_chan_file.sh "$TOP"
+./flasher_start_top.sh
+	
+# Setup/Run BOT
+caput ${BOT}:SET_COLOR $COLOR
+caput ${BOT}:DATA_FILE_LOAD HPSFlasher2${COLOR}Channels.dat
+./flasher_load_chan_file.sh "$BOT"
+./flasher_start_bot.sh
+
+exit 0
