deff34f872b9318160c8da363f21e0e587e90ca2
Hovanes Egiyan <hovanes@clonsl2.jlab.org>
2015-02-03 11:46:50 -0500

    imported xpsMotorApp from Hall D

---
+++ apps/iocBoot/iocxpsMotor/motorsHallD_save_restore.cmd
@@ -0,0 +1,15 @@
+#save_restoreSet_status_prefix("$(IOC):")
+save_restoreSet_status_prefix("")
+save_restoreSet_Debug(0)
+
+save_restoreSet_IncompleteSetsOk(1)
+save_restoreSet_DatedBackupFiles(1)
+
+save_restoreSet_NumSeqFiles(3)
+save_restoreSet_SeqPeriodInSeconds(30)
+
+set_savefile_path("./sav")
+set_requestfile_path(".")
+set_requestfile_path("../../req")
+
+
---
+++ apps/iocBoot/iocxpsMotor/motorsSVT.cmd
@@ -0,0 +1,63 @@
+#!../../bin/linux-x86/xpsMotor
+
+
+< envPaths
+
+cd ${TOP}
+
+dbLoadDatabase("dbd/xpsMotor.dbd")
+xpsMotor_registerRecordDeviceDriver(pdbbase)
+
+# Load IOC status records
+dbLoadRecords("db/iocAdminSoft.db","IOC=MTRHLD")
+
+epicsEnvSet("PREFIX","")
+### Motors
+dbLoadTemplate "db/xpsHallD.substitutions"
+dbLoadRecords "db/collimators.db", "P=US1-2-BOT:, R=m1, pref=${PREFIX}"
+dbLoadRecords "db/converters.db" , "P=US1-2-BOT:, R=m2, pref=${PREFIX}"
+
+epicsEnvSet("COLL_MOTOR","$(PREFIX)US1-2-BOT:m1")
+epicsEnvSet("CONV_MOTOR","$(PREFIX)US1-2-BOT:m2")
+# Load DB for scan using sscan EPICS record
+dbLoadRecords("db/standardScans.db","P=$(CONV_MOTOR):,MAXPTS1=2000,MAXPTS2=1000,MAXPTS3=1000,MAXPTS4=10,MAXPTSH=2000")
+dbLoadRecords("db/saveData.db","P=$(CONV_MOTOR):")
+dbLoadRecords("db/scanProgress.db","P=$(CONV_MOTOR):scanProgress:")
+
+# asyn port, IP address, IP port, number of axes, 
+# active poll period (ms), idle poll period (ms), 
+# enable set position, set position settling time (ms)
+XPSCreateController("US1-2-BOT", "us1-2-bot", 5001, 8, 10, 500, 0, 500)
+
+
+# XPS asyn port,  axis, groupName.positionerName, stepSize(1/0.00215 mm)
+# for MRES=0.0127mm  stepSize=1/0.0127
+# for integer number for stepSize , MRES = 0.012658228, stepSize = 79
+XPSCreateAxis("US1-2-BOT", 0, "Collimator.X",   "79")
+
+# for integer number for stepSize , MRES = 0.007936508, stepSize = 126
+XPSCreateAxis("US1-2-BOT", 1, "Converter.Pos",   "126")
+
+cd ${TOP}/iocBoot/${IOC}
+#asSetFilename("ca_security.txt")
+
+#< motorsHallD_save_restore.cmd
+
+#set_pass0_restoreFile("motorsHallD.sav")
+#set_pass1_restoreFile("motorsHallD.sav")
+
+#
+iocInit
+# autosave startup
+#create_monitor_set("motorsHallD.req", 30, "PREF=${PREFIX}")
+
+# Handle autosave 'commands' contained in loaded databases.
+#makeAutosaveFiles()
+#create_monitor_set("info_positions.req", 5, "P=xxx:")
+#create_monitor_set("info_settings.req", 30, "P=xxx:")
+
+# Launch sequencer for to do scan progress and save data
+cd ${TOP}/iocBoot/iocxpsMotor
+saveData_Init("saveData.req", "P=$(CONV_MOTOR):")
+seq &scanProgress, "S=$(CONV_MOTOR):,P=$(CONV_MOTOR):scanProgress:" 
+
