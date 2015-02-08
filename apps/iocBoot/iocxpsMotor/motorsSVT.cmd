fe417ffd2d28a00abaee551dda52b2252ef804d7
Hovanes Egiyan <hovanes@clonsl2.jlab.org>
2015-02-08 12:09:26 -0500

    crated an IOC to load DB for HPS target stage motor. Looked fine on motor_expert GUI

--- apps/iocBoot/iocxpsMotor/motorsSVT.cmd
+++ apps/iocBoot/iocxpsMotor/motorsSVT.cmd
@@ -9,34 +9,31 @@ dbLoadDatabase("dbd/xpsMotor.dbd")
 xpsMotor_registerRecordDeviceDriver(pdbbase)
 
 # Load IOC status records
-dbLoadRecords("db/iocAdminSoft.db","IOC=MTRHLD")
+#dbLoadRecords("db/iocAdminSoft.db","IOC=HPSXPS")
 
 epicsEnvSet("PREFIX","")
 ### Motors
-dbLoadTemplate "db/xpsHallD.substitutions"
-dbLoadRecords "db/collimators.db", "P=US1-2-BOT:, R=m1, pref=${PREFIX}"
-dbLoadRecords "db/converters.db" , "P=US1-2-BOT:, R=m2, pref=${PREFIX}"
+dbLoadTemplate "db/xpsHallB.substitutions"
+dbLoadRecords "db/targets.db", "P=HPS-XPS:, R=m1, pref=${PREFIX}"
 
-epicsEnvSet("COLL_MOTOR","$(PREFIX)US1-2-BOT:m1")
-epicsEnvSet("CONV_MOTOR","$(PREFIX)US1-2-BOT:m2")
+
+#epicsEnvSet("COLL_MOTOR","$(PREFIX)US1-2-BOT:m1")
+#epicsEnvSet("CONV_MOTOR","$(PREFIX)US1-2-BOT:m2")
 # Load DB for scan using sscan EPICS record
-dbLoadRecords("db/standardScans.db","P=$(CONV_MOTOR):,MAXPTS1=2000,MAXPTS2=1000,MAXPTS3=1000,MAXPTS4=10,MAXPTSH=2000")
-dbLoadRecords("db/saveData.db","P=$(CONV_MOTOR):")
-dbLoadRecords("db/scanProgress.db","P=$(CONV_MOTOR):scanProgress:")
+#dbLoadRecords("db/standardScans.db","P=$(CONV_MOTOR):,MAXPTS1=2000,MAXPTS2=1000,MAXPTS3=1000,MAXPTS4=10,MAXPTSH=2000")
+#dbLoadRecords("db/saveData.db","P=$(CONV_MOTOR):")
+#dbLoadRecords("db/scanProgress.db","P=$(CONV_MOTOR):scanProgress:")
 
 # asyn port, IP address, IP port, number of axes, 
 # active poll period (ms), idle poll period (ms), 
 # enable set position, set position settling time (ms)
-XPSCreateController("US1-2-BOT", "us1-2-bot", 5001, 8, 10, 500, 0, 500)
+XPSCreateController("HPS-XPS", "hpsxps", 5001, 8, 10, 500, 0, 500)
 
 
 # XPS asyn port,  axis, groupName.positionerName, stepSize(1/0.00215 mm)
 # for MRES=0.0127mm  stepSize=1/0.0127
 # for integer number for stepSize , MRES = 0.012658228, stepSize = 79
-XPSCreateAxis("US1-2-BOT", 0, "Collimator.X",   "79")
-
-# for integer number for stepSize , MRES = 0.007936508, stepSize = 126
-XPSCreateAxis("US1-2-BOT", 1, "Converter.Pos",   "126")
+XPSCreateAxis("HPS-XPS", 0, "S.TestSVT",   "79")
 
 cd ${TOP}/iocBoot/${IOC}
 #asSetFilename("ca_security.txt")
@@ -58,6 +55,6 @@ iocInit
 
 # Launch sequencer for to do scan progress and save data
 cd ${TOP}/iocBoot/iocxpsMotor
-saveData_Init("saveData.req", "P=$(CONV_MOTOR):")
-seq &scanProgress, "S=$(CONV_MOTOR):,P=$(CONV_MOTOR):scanProgress:" 
+#saveData_Init("saveData.req", "P=$(CONV_MOTOR):")
+#seq &scanProgress, "S=$(CONV_MOTOR):,P=$(CONV_MOTOR):scanProgress:" 
 
