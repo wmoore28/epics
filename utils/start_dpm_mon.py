11f0e794a4946fbbbca4baa89d080ccc44c6cae4
perhansson <per.hansson80@gmail.com>
2015-02-28 17:50:25 -0500

    Utility script to handle monitor procServ for the SVT

--- utils/start_dpm_mon.py
+++ utils/start_dpm_mon.py
@@ -7,66 +7,66 @@ import sys
 
 parser = OptionParser()
 #parser.add_option("-n","--nodes", dest="nodes", help="nodes", nargs=">=0")
+parser.add_option("-t", action="store_true", dest="test")
 (options, args) = parser.parse_args()
 print options
 print args
-
 
 xpos = 10
 ypos = 0
 xinc = 510
 yinc = 120
 lines = 8
-
 color = "-bg green -fg black -sb -sl 5000"
 geometry  = "-geometry 70x" + str(lines)
-#targetDir = '/u1/epics/svt-epics/svt-daq-epics/example/iocBoot/iocsvtDaqDpm0'
-#targetCntrlDpmDir = '/u1/epics/svt-epics/svt-daq-epics/example/iocBoot/iocmyexampleApp'
 
-# print IOC status
-ioc_status_cmd = "procServMgr -p /usr/clas12/hps/dev/apps/iocBoot status"
-subprocess.call(ioc_status_cmd)
+procServConf = "/usr/clas12/hps/dev/apps/iocBoot/procServ.conf"
+iocNames = {}
+for l in open(procServConf,"r").readlines():
+    if "svtDaq" in l:
+        iocNames[l.split()[0].replace(":","")] = l.split()[2].replace(":","")
 
-#ypos = yinc
-for dpm_index in range (0, 1):
-    ioc_name = "iocchiller"
-    #ioc_name = "iocsvtDaqDpm%d" % (dpm_index)
-	ix = dpm_index % 3
-	iy = math.floor(dpm_index/3)
-	xpos = ix*xinc
-	ypos = yinc + iy*yinc
-	#print "x ", ix, " -> ", xpos, " y ", iy, " -> ", ypos
-	position = geometry + "+" + str(xpos) + "+" + str(int(ypos))
-	dpm = "dpm" + str(dpm_index)
-	if len(args)>0 and dpm not in args:
-		print "skip dpm ", str(dpm_index)
-		continue
-	title = "-T DPM" + str(dpm_index)
-	exeName = "svtDaqDpm" + str(dpm_index) + ".cmd"
-	command = "xterm " + title + " " + color + " " + position
-	command += " -e 'softioc_console " + iocname + ";bash'" 
-	print command
-	#subprocess.Popen(command + "&", shell=True)
-    continue
-	if dpm_index == 0 or dpm_index == 1:
-		xpos = (dpm_index+1)*510
-		ypos = 0
-		dtm = "dtm" + str(dpm_index)
-		title = "-T DTM" + str(dpm_index) + "IOC"
-		position = geometry + "+" + str(int(xpos)) + "+" + str(int(ypos))
-		exeName = "svtDaqDtm" + str(dpm_index) + ".cmd"
-		command = "xterm " + title + " " + color + " " + position
-		command += " -e 'pushd " + targetDir+ ";./" + exeName + ";bash'" 		
-		#print command
-		subprocess.Popen(command + "&", shell=True)
-	if dpm_index == 0:
-		xpos = 0
-		ypos = 0
-		dtm = "cntrldpm"
-		title = "-T CntrlDpmIOC"
-		position = geometry + "+" + str(int(xpos)) + "+" + str(int(ypos))
-		exeName = "st.cmd"
-		command = "xterm " + title + " " + color + " " + position
-		command += " -e 'pushd " + targetCntrlDpmDir + ";./" + exeName + ";bash'" 		
-		#print command
-		subprocess.Popen(command + "&", shell=True)
+
+print "SVT DAQ IOC's: ", iocNames 
+
+
+for dpm in range (0, 17):
+
+    ix = dpm % 3
+    iy = math.floor(dpm/3)
+    xpos = ix*xinc
+    ypos = yinc + iy*yinc
+    #print "x ", ix, " -> ", xpos, " y ", iy, " -> ", ypos
+    position = geometry + "+" + str(xpos) + "+" + str(int(ypos))
+
+    name = "dpm" + str(dpm)
+    iocName =  "iocsvtDaqDpm" + str(dpm)
+    if dpm==14:
+        name = "controldpm"
+        iocName = "iocsvtDaq"
+    if dpm==15:
+        name = "dtm0"
+        iocName = "iocsvtDaqDtm0"
+    if dpm==16:
+        name = "dtm1"
+        iocName = "iocsvtDaqDtm1"
+
+    go = True    
+    if len(args)>0 and name not in args:
+        go = False
+    
+    if go:
+        cmd = "telnet localhost " + iocNames[iocName]
+        title = "-T " + iocName
+        command = "xterm " + title + " " + color + " " + position
+        command += " -e '" + cmd + ";bash'" 
+        print command
+        if not options.test:
+            #process = subprocess.Popen(command + "&",shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            #out, err = process.communicate()
+            #errcode = process.returncode
+            #print out
+            #if err is not None:
+            #    print err
+            #print "Error code: ", errcode
+            process = subprocess.Popen(command + "&",shell=True)
