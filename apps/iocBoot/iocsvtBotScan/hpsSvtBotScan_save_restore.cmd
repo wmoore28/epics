00d0b1cf30aa9a21c221d5a5a4e57e750e8c96f8
Wesley Moore <wmoore@jlab.org>
2015-03-01 15:51:37 -0500

    xpsMotorApp iocs: updated save_restore config to point to IOCDATA

--- apps/iocBoot/iocsvtBotScan/hpsSvtBotScan_save_restore.cmd
+++ apps/iocBoot/iocsvtBotScan/hpsSvtBotScan_save_restore.cmd
@@ -1,15 +1,13 @@
-#save_restoreSet_status_prefix("$(IOC):")
-save_restoreSet_status_prefix("")
-save_restoreSet_Debug(0)
+  ## General settings
+  #save_restoreSet_status_prefix("$(IOC):")
+  save_restoreSet_status_prefix("")
+  save_restoreSet_Debug(0)
+  save_restoreSet_IncompleteSetsOk(1)
+  save_restoreSet_DatedBackupFiles(1)
+  save_restoreSet_NumSeqFiles(3)
+  save_restoreSet_SeqPeriodInSeconds(30)
 
-save_restoreSet_IncompleteSetsOk(1)
-save_restoreSet_DatedBackupFiles(1)
-
-save_restoreSet_NumSeqFiles(3)
-save_restoreSet_SeqPeriodInSeconds(30)
-
-set_savefile_path("./sav")
-set_requestfile_path(".")
-set_requestfile_path("../../req")
-
-
+  ## Specify paths
+  set_savefile_path("$(IOCDATA)/autosave/$(IOC)")
+  set_requestfile_path(".")
+  set_requestfile_path("$(TOP)/req")
