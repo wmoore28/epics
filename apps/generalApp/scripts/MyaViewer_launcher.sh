18f2ae5f74485151257b185b54fa443cc76812c0
Wesley Moore <wmoore@jlab.org>
2015-03-10 17:13:06 -0400

    MyaViewer: added host arg to launcher and updated HPS Main

--- apps/generalApp/scripts/MyaViewer_launcher.sh
+++ apps/generalApp/scripts/MyaViewer_launcher.sh
@@ -6,6 +6,15 @@
 host=clonpc19
 user=hpsrun
 
+while getopts h: opt; do
+  case $opt in
+  h)
+    host=$OPTARG
+    ;;
+  esac
+done
+shift $((OPTIND - 1))
+
 echo "Running MyaViewer from $host..."
 read -p "Enter your username [$user]: " INPUT_STRING
 if [ "$INPUT_STRING" != "" ]; then
