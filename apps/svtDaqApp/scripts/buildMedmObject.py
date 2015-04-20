b32284519f7ab3fe7b64b9927251bc6235324784
perhansson <per.hansson80@gmail.com>
2015-04-20 15:14:32 -0400

    Add indicators for hybrid sync

--- apps/svtDaqApp/scripts/buildMedmObject.py
+++ apps/svtDaqApp/scripts/buildMedmObject.py
@@ -61,8 +61,49 @@ oval {
 
 """
 
-XBASE=704
-YBASE=74
+
+template2 = """
+
+oval {
+	object {
+		x=XPOS
+		y=YPOS
+		width=20
+		height=20
+	}
+	"basic attribute" {
+		clr=20
+	}
+	"dynamic attribute" {
+		vis="calc"
+		calc="A=21"
+		chan="CHA"
+	}
+}
+
+oval {
+	object {
+		x=XPOS
+		y=YPOS
+		width=20
+		height=20
+	}
+	"basic attribute" {
+		clr=15
+	}
+	"dynamic attribute" {
+		vis="calc"
+		calc="A!21"
+		chan="CHA"
+	}
+}
+
+"""
+
+
+
+XBASE=200
+YBASE=62
 WIDTH=20
 HEIGHT=20
 
@@ -84,18 +125,33 @@ HEIGHT=20
 #            print t
 
 
-for dpm in range(15):
-    X = XBASE + 25 
-    Y = YBASE + dpm*25
-    CHA = "SVT:daq:dpm:" + str(dpm) + ":blockcount"
-    CHB = "SVT:daq:dtm:0:readcount"
-    t = template1    
-    t=t.replace("XPOS",str(X))
-    t=t.replace("YPOS",str(Y))    
-    t=t.replace("CHA",CHA)
-    t=t.replace("CHB",CHB)
-    t=t.replace("WIDTH",str(WIDTH))
-    t=t.replace("HEIGHT",str(HEIGHT))
-    print t
+#for dpm in range(15):
+#    X = XBASE + 25 
+#    Y = YBASE + dpm*25
+#    CHA = "SVT:daq:dpm:" + str(dpm) + ":blockcount"
+#    CHB = "SVT:daq:dtm:0:readcount"
+#    t = template1    
+#    t=t.replace("XPOS",str(X))
+#    t=t.replace("YPOS",str(Y))    
+#    t=t.replace("CHA",CHA)
+#    t=t.replace("CHB",CHB)
+#    t=t.replace("WIDTH",str(WIDTH))
+#    t=t.replace("HEIGHT",str(HEIGHT))
+#    print t
+
+i = 0
+for feb in range(10):
+    for hyb in range(4):
+        X = XBASE 
+        Y = YBASE + i*25
+        CHA = "SVT:lv:"+str(feb)+":" + str(hyb) + ":sync:sync_rd"
+        t = template2    
+        t=t.replace("XPOS",str(X))
+        t=t.replace("YPOS",str(Y))    
+        t=t.replace("CHA",CHA)
+        t=t.replace("WIDTH",str(WIDTH))
+        t=t.replace("HEIGHT",str(HEIGHT))
+        i = i + 1
+        print t
 
 
