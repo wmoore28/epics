865111a56b68bb15b33b631e5f98f84e19370c27
baltzell <baltzell@gmx.com>
2015-07-28 15:13:44 -0400

    updating A6621 st.cmd

--- apps/iocBoot/iocA6621/st.cmd
+++ apps/iocBoot/iocA6621/st.cmd
@@ -11,11 +11,16 @@ cd ${TOP}
 dbLoadDatabase "dbd/A6621.dbd"
 A6621_registerRecordDeviceDriver pdbbase
 
+## Configure devices
+drvAsynIPPortConfigure("L0",129.57.160.95:1234,0,0,0)
+#asynSetTraceMask("L0",-1,0x09)
+#asynSetTraceIOMask("L0",-1,0x02)
+asynOctetSetOutputEos("L0",0,"\r\n")
+asynOctetSetInputEos("L0",0,"\n")
+
 ## Load record instances
-#dbLoadRecords("db/xxx.db","user=baltzellHost")
+dbLoadRecords("./A6621.db","PV=HPSECALLV,PORT=L0,ADDR=24,IMAX=2000,OMAX=2000")
 
 cd ${TOP}/iocBoot/${IOC}
 iocInit
 
-## Start any sequence programs
-#seq sncxxx,"user=baltzellHost"
