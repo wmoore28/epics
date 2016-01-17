dec2102e114c0b3b752da44b295384fea9f438e1
baltzell <baltzell@gmx.com>
2016-01-17 12:25:55 -0500

    chicaneApp:  adding autosave (calculated magnet currents and corresponding beam energy, hold time, current tolerance)

--- apps/iocBoot/iocchicane/st.cmd
+++ apps/iocBoot/iocchicane/st.cmd
@@ -8,10 +8,16 @@ chicane_registerRecordDeviceDriver pdbbase
 
 dbLoadRecords "db/chicane.db", "P=hps_chicane"
 dbLoadRecords("db/iocAdminSoft.db", "IOC=${IOC}")
+dbLoadRecords("db/save_restoreStatus.db", "P=${IOC}:")
 
 cd ${TOP}/iocBoot/${IOC}
 
+< save_restore.cmd
+
 iocInit
+
+makeAutosaveFiles()
+create_monitor_set("chicane_settings.req",30,"P=hps_chicane")
 
 seq seqChicane, "P=hps_chicane"
 
