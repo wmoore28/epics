4f408c67201d118f915757841e9f1519dc1f7ad5
perhansson <per.hansson80@gmail.com>
2016-02-11 15:07:37 -0500

    set sverity to make the alarms actually do anything

--- apps/svtDaqApp/scripts/setSemAlarms.sh
+++ apps/svtDaqApp/scripts/setSemAlarms.sh
@@ -3,10 +3,10 @@
 
 base="SVT:daq:feb"
 names="sem_error_stat sem_heartbeat_stat"
-lim_high=(0.0 1.0)
-lim_hihi=(0.0 1.0)
-lim_low=(0.0 1.0)
-lim_lolo=(0.0 1.0)
+lim_high=(1 2)
+lim_hihi=(1 2)
+lim_low=(-1 0)
+lim_lolo=(-1 0)
 
 iname=0
 for n in $names
@@ -19,6 +19,10 @@ do
 	caput "$name.LOLO" ${lim_lolo[iname]}
 	caput "$name.HIGH" ${lim_high[iname]}
 	caput "$name.HIHI" ${lim_hihi[iname]}
+	caput "$name.LSV" MINOR
+	caput "$name.HSV" MINOR
+	caput "$name.LLSV" MAJOR
+	caput "$name.HHSV" MAJOR
     done
     ((iname = $iname + 1))
 done
