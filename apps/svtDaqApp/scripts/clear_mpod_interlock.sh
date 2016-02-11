379ffb07e07009e723e039ad42d18e31ea1ed053
perhansson <per.hansson80@gmail.com>
2016-02-10 20:02:47 -0500

    hardcode path. Not sure this is a good thing.

--- apps/svtDaqApp/scripts/clear_mpod_interlock.sh
+++ apps/svtDaqApp/scripts/clear_mpod_interlock.sh
@@ -2,6 +2,6 @@
 
 for j in `seq 5 8`; do
     for i in `seq -w 00 15`; do
-        snmpset -v2c -m +WIENER-CRATE-MIB -c  guru hpsmpod outputSwitch.u$j$i i 10
+        snmpset -v2c -m +WIENER-CRATE-MIB -M +/usr/clas12/hps/dev/apps/iocBoot/iochvCaen/ -c guru hpsmpod outputSwitch.u$j$i i 10
     done
 done
