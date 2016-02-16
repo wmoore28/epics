a33d642cb7557ab9f5657ea293060f208944af9a
baltzell <baltzell@gmx.com>
2016-02-16 18:54:13 -0500

    hpsTiPrescale.sh:  fix arg checking

--- bin/hpsTiPrescale.sh
+++ bin/hpsTiPrescale.sh
@@ -4,21 +4,24 @@ trigger=$1
 prescale=$2
 usage='hpsTiPrescale.sh trigger[1-6] prescale[0-15]'
 
-if [[ -z "$1" || -z "$2" ]]
+if ! [[ -n "$1" && -n "$2" ]]
 then
   echo $usage
+  exit
 fi 
 
 if [ "$prescale" -lt 0 ] || [ "$prescale" -gt 15 ]
 then
   echo Invalid Prescale:  $prescale
   echo $usage
+  exit
 fi
 
 if [ "$trigger" -lt 1 ] || [ "$trigger" -gt 6 ]
 then
   echo Invalid Trigger:  $trigger
   echo $usage
+  exit
 fi
 
 let epicsTrigger=$trigger-1
