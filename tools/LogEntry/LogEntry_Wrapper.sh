9dd558cd593d2cbdebd19962de3bbf2b3376205e
Rafayel Paremuzyan <rafopar@jlab.org>
2016-03-28 16:16:28 -0400

    This shell scrpits sets JAVA_HOME and PATH to a working version of Java, i.e. it forses to use the selected version ov Java

---
+++ tools/LogEntry/LogEntry_Wrapper.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# force java version
+
+if [ "$HOSTNAME" == "clonpc19.jlab.org" ]; then
+echo "Running on clonpc19"
+  #export JAVA_HOME=/usr/lib/jvm/java-1.8.0-oracle.x86_64
+  #export JAVA_HOME=/usr/lib/jvm/java-1.7.0-oracle.x86_64
+  #export PATH=$JAVA_HOME/bin:$PATH
+else
+  export JAVA_HOME=/usr/lib/jvm/java-1.8.0-oracle
+  export PATH=$JAVA_HOME/bin:$PATH
+fi
+
+echo "Java jome = $JAVA_HOME"
+
+java org/jlab/AutoLogEntry/MakeLogEntry $1 $2 
