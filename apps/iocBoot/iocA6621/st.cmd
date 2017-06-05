8e6abb3720be406bf2dc468a32d429823a638d76
baltzell <baltzell@gmx.com>
2017-06-05 13:36:18 -0400

    iocA6621:  remove asyn record from A6621.db

--- apps/iocBoot/iocA6621/st.cmd
+++ apps/iocBoot/iocA6621/st.cmd
@@ -15,16 +15,18 @@ epicsEnvSet("STREAM_PROTOCOL_PATH","${TOP}/proto")
 
 ## Configure devices
 drvAsynIPPortConfigure("L0",129.57.160.95:1234,0,0,0)
-#drvAsynIPPortConfigure("L0",hallb-gpib01.jlab.org:1234,0,0,0)
 
-#asynSetTraceMask("L0",-1,0x09)
-#asynSetTraceIOMask("L0",-1,0x02)
+asynSetTraceMask("L0",-1,0x09)
+asynSetTraceIOMask("L0",-1,0x02)
 asynOctetSetOutputEos("L0",0,"\r\n")
 asynOctetSetInputEos("L0",0,"\n")
 
 ## Load record instances
 ## Call one for each with sector, layer and GPIB ID
-dbLoadRecords("db/A6621.db","P=HPSECALLV,PORT=L0,ADDR=24,IMAX=2000,OMAX=2000")
+#dbLoadRecords("db/A6621.db","P=HPSECALLV,PORT=L0,ADDR=1,IMAX=2000,OMAX=2000")
+dbLoadRecords("db/A6621.db","P=HPSECALLV,PORT=L0")
+dbLoadRecords("db/asynRecord.db","P=HPSECALLV,R=:ASYN,PORT=L0,ADDR=1,IMAX=2000,OMAX=2000")
+
 
 dbLoadRecords("db/save_restoreStatus.db", "P=${IOC}:")
 
